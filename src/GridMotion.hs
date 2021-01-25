23093ebd49759b538c54ead3232b007b047c80a7
James Carlson <carlson@jxxM1.local>
2021-01-25 02:15:37 -0500

    GridMotion game in current (primitive) form now functions.

--- src/GridMotion.hs
+++ src/GridMotion.hs
@@ -5,36 +5,47 @@ import Control.Monad.State
 
 data Position = P Int Int deriving Show
 
-applyFirst :: (Int -> Int) -> Position -> Position
-applyFirst f (P x y) = P (f x) y
+applyX :: (Int -> Int) -> Position -> Position
+applyX f (P x y) = P (f x) y
 
-applySecond :: (Int -> Int) -> Position -> Position
-applySecond f (P x y) = P x (f y)
+applyY :: (Int -> Int) -> Position -> Position
+applyY f (P x y) = P x (f y)
+
+applyXGS :: (Int -> Int) -> GameState -> GameState
+applyXGS f gs = gs { current = applyX f (current gs)}
+
+applyYGS :: (Int -> Int) -> GameState -> GameState
+applyYGS f gs = gs { current = applyY f (current gs)}
 
 distance :: Position -> Position -> Int
 distance (P x y) (P x' y') = abs (x - x') + abs (y - y')
 
-play :: String -> State Position Position
+data GameState  = GameState {current :: Position, goal :: Position}
+
+play :: String -> State GameState Position
 play []     = do
-    get
+    gs <- get
+    return (current gs)
 
 play (x:xs) = do
-    pos <- get
-    case x of
-         'n' -> put $ applySecond (+1) pos
-         'w' -> put $ applyFirst (\x -> x - 1) pos
-         's' -> put $ applySecond (\y -> y - 1) pos
-         'e' -> put $ applyFirst (+1) pos
-         _ -> put pos
+    gs <- get
+    let gs' = case x of
+         'n' -> applyYGS (+1) gs
+         'w' -> applyXGS (\x -> x - 1) gs
+         's' -> applyYGS (\y -> y - 1) gs
+         'e' -> applyXGS (+1) gs
+         _ -> gs 
+    put gs'    
     play xs
 
-startState = P 0 0
+startState x y = GameState {current = P 0 0, goal = P x y}
 
 playGM = do
     putStrLn "Enter final position, x:"
-    xStr <- getLine 
+    xStr<- getLine 
     putStrLn "Enter final position, x:"
-    xStr <- getLine 
+    yStr <- getLine 
     putStrLn "Enter directions, e.g., nnwwwwn"
     input <- getLine
-    print $ evalState (play input) startState
+    print $ evalState (play input) (startState (read xStr :: Int)  (read yStr :: Int))
+    -- print $ evalState (play input) (startState 5  7)
